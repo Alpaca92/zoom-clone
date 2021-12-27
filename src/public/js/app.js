f9e2f728b8c074d882b5b3ac6dfc40426c37e4fb
alpaca <zxcvbnm5288@gmail.com>
2021-12-27 17:44:50 +0900

    #1.4: bi-connectional, send and receive message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
-alert('hi')
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => console.log(`Connected to Server ✔`));
+socket.addEventListener("message", (event) => {
+  console.log(event.data);
+});
+socket.addEventListener("close", () =>
+  console.log(`Disconnected to Server ❌`)
+);
+
+setTimeout(() => {
+  socket.send(`Hello from the browser`);
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
 import express from "express";
 
 const app = express();
@@ -7,11 +9,21 @@ app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
 const handleListen = () => {
   console.log(`Listening on http://localhost:3000`);
 };
 
-app.listen(3000, handleListen);
+const server = http.createServer(app); // http server
+const wss = new WebSocketServer({ server }); // ws server on top http server
+
+wss.on("connection", (socket) => {
+  console.log(`Connected to Browser ✔`);
+  socket.on("close", () => console.log(`Disconnected from the Browser ❌`));
+  socket.send(`hello`);
+  socket.on("message", (message) => console.log(message.toString("utf8")));
+});
+
+server.listen(3000, handleListen);
